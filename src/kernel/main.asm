b2899ba860c212aeccb41a5391a67089b9436d67
Ansh_sharma <keshu1234harshu@gmail.com>
2024-04-18 15:47:03 +0530

    added fat file system

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,23 +1,37 @@
-org 0x7C00
+org 0x0
 bits 16
+
 
 %define ENDL 0x0D, 0x0A
 
-start:
-    jmp main
 
+start:
+    ; print hello world message
+    mov si, msg_hello
+    call puts
+
+.halt:
+    cli
+    hlt
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
 puts:
+    ; save registers we will modify
     push si
     push ax
     push bx
 
 .loop:
-    lodsb
-    or al, al
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
     jz .done
 
-    mov ah, 0x0E
-    mov bh, 0
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
     int 0x10
 
     jmp .loop
@@ -28,23 +42,4 @@ puts:
     pop si    
     ret
 
-main:
-    mov ax, 0
-    mov ds, ax
-    mov es, ax
-    
-    mov ss, ax
-    mov sp, 0x7C00
-
-    mov si, msg_hello
-    call puts
-
-    hlt
-
-.halt:
-    jmp .halt
-
-msg_hello: db 'Hello world from kernel!', ENDL, 0
-
-times 510-($-$$) db 0
-dw 0AA55h
+msg_hello: db 'Hello world from KERNEL!', ENDL, 0
