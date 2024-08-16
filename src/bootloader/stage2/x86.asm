8dbdb8136e9a844843057bda6cb24de2f8bf005d
Ansh_sharma <keshu1234harshu@gmail.com>
2024-08-17 00:04:08 +0530

    some problem fixed

--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -2,23 +2,37 @@ bits 16
 
 section _TEXT class=CODE
 
+global _x86_div64_32
+_x86_div64_32:
+    push bp
+    mov bp, sp
+    push bx
+    mov eax, [bp + 8]
+    mov ecx, [bp + 12]
+    xor edx, edx
+    div ecx
+    mov bx, [bp + 16]
+    mov [bx + 4], eax
+    mov eax, [bp + 4]
+    div ecx
+    mov [bx], eax
+    mov bx, [bp + 18]
+    mov [bx], edx
+    pop bx
+    mov sp, bp
+    pop bp
+    ret
+
 global _x86_Video_WriteCharTeletype
 _x86_Video_WriteCharTeletype:
-    
-    
-    push bp             
-    mov bp, sp          
-
+    push bp
+    mov bp, sp
     push bx
-
     mov ah, 0Eh
     mov al, [bp + 4]
     mov bh, [bp + 6]
-
     int 10h
-
     pop bx
-
     mov sp, bp
     pop bp
     ret
