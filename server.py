d8e2f5b87cca1db1cd66ea1b2da7b265e8252fe6
Ansh_sharma <keshu1234harshu@gmail.com>
2024-11-10 01:44:19 +0530

    test

---
+++ server.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+def run_command(command):
+    """ Run a shell command and stream the output in real-time """
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True)
+    
+    # Stream the stdout and stderr
+    for stdout_line in process.stdout:
+        yield f"data: {stdout_line}\n\n"
+    
+    for stderr_line in process.stderr:
+        yield f"data: {stderr_line}\n\n"
+    
+    process.stdout.close()
+    process.stderr.close()
+    process.wait()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/build', methods=['POST'])
+def build():
+    # Stream the output of 'make' and 'run.sh'
+    return Response(run_command(['make']), content_type='text/event-stream')
+
+@app.route('/run', methods=['POST'])
+def run():
+    # Stream the output of 'run.sh'
+    return Response(run_command(['bash', './run.sh']), content_type='text/event-stream')
+
+if __name__ == '__main__':
+    app.run(debug=True)
